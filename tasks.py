3a1e3da92da4021efe467544ded3a8f41bb15112
Janusz Skonieczny <git@wooyek.com>
2015-11-19 19:59:15 +0100

    init

---
+++ tasks.py
@@ -0,0 +1,32 @@
+# coding=utf-8
+from shutil import copy
+from time import sleep
+
+from pathlib import Path
+from invoke import run, task
+
+@task
+def tags(docs=False):
+    if docs:
+        run(u"Przygotuj tagi dla pliku pong.py w oparciu o wersje pliku w folderze zadania")
+
+    targets = {}
+    cwd = Path(".")
+    exercises = Path("./zadania")
+    for p in exercises.iterdir():
+        name, tag = p.stem.rsplit("_", 1)
+        targets.setdefault(tag, []).append(name)
+    for tag in sorted(targets.keys()):
+        for name in targets[tag]:
+            src = exercises / (name + "_" + tag + ".py")
+            dst = cwd / (name+".py")
+            print "Kopiuje ", src, " do ", dst
+            copy(unicode(src), unicode(dst))
+        run('git commit -am '+tag)
+        run('git tag -f '+tag)
+        sleep(1)  # Git psuje ścieżki powiązań jeśli operacje są zbyt blisko siebie
+
+
+@task
+def push(docs=False):
+    run("git push --tags")
