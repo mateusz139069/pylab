3c8f6700f3e6cca53f65bbf5b2bff307907cdec8
xinulsw <xinulsw@gmail.com>
2015-12-06 21:27:20 +0100

    z03

--- fun2.py
+++ fun2.py
@@ -13,7 +13,7 @@ y1 = [i / -3 + a for i in x if i <= 0]
 
 y2 = [i**2 / 3 for i in x if i >= 0]
 
-pylab.plot(x[:len(y1)], y1, x, y2)
+pylab.plot(x[:len(y1)], y1, x[-len(y2):], y2)
 pylab.title('Wykres f(x)')
 pylab.grid(True)
 pylab.show()
